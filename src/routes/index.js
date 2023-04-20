cd48f294426ec2105ddc7f75d00bfa032094ac40
fevgaray <fev.garay@gmail.com>
2023-04-20 14:34:11 -0400

    Entrega Cert 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import Router from 'koa-router'
 import getHealth from './health/health'
-
+import getNameByAge from './users/users'
+import getUserDetails from './users/users'
 const router = new Router()
 
 router.get('/health', getHealth)
 
+router.get('/api/users/:name', getNameByAge)
+
+router.get('api/users', getUserDetails)
 export default router
