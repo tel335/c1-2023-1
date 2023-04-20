12403f51e37771fb310d56c5b198fa095a4ffab7
bzyyy <brian.montero@sansano.usm.cl>
2023-04-20 14:41:07 -0400

    Resolución Certamen 1 Brian Montero 201830015-2

--- src/repository/usersRepository.js
+++ src/repository/usersRepository.js
@@ -1,5 +1,24 @@
+import users from '../dataset/users_info.json'
 import dataset from '../dataset/users_info.json'
 
-export default function getUsersFromDataset () {
-    return dataset
+function getUsersByName(name){
+    const filteredUsers = users.filter(user => user.name.toLowerCase().includes(name) && user.isActive);
+    const sortedUsers = filteredUsers.sort((a, b) => b.age - a.age);
+    return sortedUsers;
 }
+
+function detailUser (age, name, geneder, company, isActive) {
+    const user = users.find(user =>
+        user.age === age &&
+        user.name === name &&
+        user.gender === gender &&
+        user.company === company &&
+        user.isActive === isActive
+        )
+    return user || null
+}
+
+module.exports = {
+    getUsersByName,
+    detailUser,
+  };
