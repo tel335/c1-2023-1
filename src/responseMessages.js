bb84493d6ac2c07412a6376f6e2e955a2671d1b2
Martín Rojas Moya <mrojas@MacBook-Pro-de-Martin.local>
2023-04-20 15:01:24 -0400

    Entrega

---
+++ src/responseMessages.js
@@ -0,0 +1,13 @@
+//Archivo para el manejo de los mensajes de respuestas 
+exports.error_message =  {
+    status: 500,
+    message: "Error en el formato de los datos"
+}
+exports.not_found_message = {
+    status: 200,
+    message: "No se encontró el usuario buscado"
+}
+exports.input_error = {
+    status: 500,
+    message: 'La entrada del parámetro presenta un error'
+}
